8e2ea1e4194d10c90c9eab69ffadadf7079cd1d6
centek134 <centek134@wp.pl>
2021-05-03 19:31:56 +0200

    some Changes

--- src/containers/QuizzPage/QuizPage.js
+++ src/containers/QuizzPage/QuizPage.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import './QuizPage.css';
 import Question from '../../components/Question/Question.js';
+import CategoryCard from '../../components/CategoryCard/CategoryCard.js';
+
+
+
 
 
 class QuizPage extends Component {
+
+
+    DataFetch = () => {
+        fetch('https://opentdb.com/api_category.php')
+        .then(result => result.json())
+        .then(data => console.log(data));git
+    };
+
 
 
     render = () =>{
         return(
             <div className = "QuizPage">
                 <Question/>
+                <CategoryCard categoryName = {"Mithology artists"}/>
+                <button onClick = {this.DataFetch}> fetch data test</button>
             </div>
         );
     };
