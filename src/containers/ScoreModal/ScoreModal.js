728a32967fb7e57c02771194a1ba9834fdcf7736
centek134 <centek134@wp.pl>
2021-05-17 11:25:17 +0200

    Changes Switched switch statemant to ifs added buttons

--- src/containers/ScoreModal/ScoreModal.js
+++ src/containers/ScoreModal/ScoreModal.js
@@ -1,30 +1,35 @@
 import React from 'react';
-
+import './ScoreModal.css';
 const ScoreModal = (props) =>{
+
     let endText = '';
-    switch(props.completion){
-        case 100:
+  
+        if(props.completion === 100){
             endText = 'Excellent!'
-            break;
-        case (100 > props.completion >= 80):
+        }
+        else if(10 > props.completion && props.completion >= 8){
             endText = 'Well done!';
-            break;
-        case  (80 > props.completion >= 60):
+        }
+        else if(8 > props.completion && props.completion >= 6){
             endText = 'Quite good!';
-            break;
-        case (60 > props.completion >= 40):
-            endText = 'Resonable score.'
-            break;
-        case ( 40 > props.completion):
+        }
+        else if(6 > props.completion && props.completion >= 4){
+            endText = 'Resonable score.';
+        }
+        else if( 4 > props.completion){
             endText = 'You should try again ;-;';
-            break;
-        default:
-            return;
-    }
+        };
+    
     return(
-        <div>
-            <h2>{endText}</h2>
-            <h3>Score: {props.completion}% / 100%</h3>
+        <div className = 'score-modal'>
+            <div className = 'container'>
+                <h2>{endText}</h2>
+                <h3>Score: {props.completion} / 10</h3>
+                <div className = 'flex-btns'>
+                    <button onClick = {props.check}>Check answers</button>
+                    <button onClick = {props.continue}>Continue</button>
+                </div>
+            </div>
         </div>
     );
 };
